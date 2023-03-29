f0ea7636932be4ad7395ca46fc97d5455ae7fcc8
Evan Shortiss <evanshortiss@gmail.com>
2023-03-29 11:00:07 -0700

    feat: simplify under pressure on/off toggle chore: change under pressure defaults for elu and lag

--- src/config.ts
+++ src/config.ts
@@ -8,6 +8,7 @@ export type AppEnvionmentVariables = {
   MAX_THREADS?: number
   MAX_EVENT_LOOP_DELAY: number
   MAX_EVENT_LOOP_UTILIZATION: number
+  UNDER_PRESSURE_ENABLED: boolean
 }
 
 export type AppOptions = {
@@ -21,7 +22,9 @@ export function getConfig (env: NodeJS.ProcessEnv): AppEnvionmentVariables {
   return {
     USE_THREADS: get('USE_THREADS').default('false').asBool(),
     MAX_THREADS: get('MAX_THREADS').asIntPositive(),
-    MAX_EVENT_LOOP_DELAY: get('MAX_EVENT_LOOP_DELAY').default('900').asIntPositive(),
-    MAX_EVENT_LOOP_UTILIZATION: get('MAX_EVENT_LOOP_UTILIZATION').default('0.9').asFloatPositive()
+    MAX_EVENT_LOOP_DELAY: get('MAX_EVENT_LOOP_DELAY').default('950').asIntPositive(),
+    MAX_EVENT_LOOP_UTILIZATION: get('MAX_EVENT_LOOP_UTILIZATION').default('0.95').asFloatPositive(),
+    UNDER_PRESSURE_ENABLED: get('UNDER_PRESSURE_ENABLED').default('true').asBool()
+    
   }
 }
--- src/plugins/under-pressure.ts
+++ src/plugins/under-pressure.ts
@@ -3,20 +3,25 @@ import up, { UnderPressureOptions } from '@fastify/under-pressure'
 import { AppOptions } from '../config'
 
 export default fp<AppOptions>(async (fastify, options) => {
-  const { MAX_EVENT_LOOP_DELAY, MAX_EVENT_LOOP_UTILIZATION } = options.config
-  const opts: UnderPressureOptions = {
-    maxEventLoopDelay: MAX_EVENT_LOOP_DELAY,
-    maxEventLoopUtilization: MAX_EVENT_LOOP_UTILIZATION,
-    pressureHandler: (req, reply, type, value) => {
-      if (req.url.includes('/probes/')) {
-        // Allow the kube probe endpoints to pass through
-        fastify.log.warn(`under pressure allowing passthrough for probe ${req.url} despite pressure of type ${type}`)
-      } else {
-        fastify.log.warn(`under pressure of type "${type}". returning 503 response`)
-        reply.serviceUnavailable('This service is currently overwhelmed. Try again later.')
+  const { MAX_EVENT_LOOP_DELAY, MAX_EVENT_LOOP_UTILIZATION, UNDER_PRESSURE_ENABLED } = options.config
+  
+  if (UNDER_PRESSURE_ENABLED) {
+    const opts: UnderPressureOptions = {
+      maxEventLoopDelay: MAX_EVENT_LOOP_DELAY,
+      maxEventLoopUtilization: MAX_EVENT_LOOP_UTILIZATION,
+      pressureHandler: (req, reply, type, value) => {
+        if (req.url.includes('/probes/')) {
+          // Allow the kube probe endpoints to pass through
+          fastify.log.warn(`under pressure allowing passthrough for probe ${req.url} despite pressure of type ${type}`)
+        } else {
+          fastify.log.warn(`under pressure of type "${type}". returning 503 response`)
+          reply.serviceUnavailable('This service is currently overwhelmed. Try again later.')
+        }
       }
-    }
-  } 
+    } 
 
-  fastify.register(up, opts)
+    fastify.register(up, opts)
+  } else {
+    fastify.log.warn('UNDER_PRESSURE_ENABLED was false. under pressure plugin not registered')
+  }
 })
